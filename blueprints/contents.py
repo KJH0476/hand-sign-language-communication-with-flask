ea42387249342fccfaa9764958ccc91b662de1e4
KJH0476 <148829100+KJH0476@users.noreply.github.com>
2024-05-02 13:06:45 +0900

    chore: 입력텍스트 소켓 전송 로직 누락 확인 및 수정
    사용자가 입력한 텍스트를 분리한 후 언리얼 클라이언트 측으로 소켓 전송하는 부분이 없다는것을 확인하고 소켓 전송 로직 추가함

--- blueprints/contents.py
+++ blueprints/contents.py
@@ -9,12 +9,13 @@ def text_show():
     data = request.get_json()
     input_word = data['text']
 
-    print('input word:', input_word)
-
     result = separate_text(input_word)
     print(result)
 
-    #입력한 텍스트 화면에서 확인할 수 있도록 소켓 전송
+    # 입력 텍스트 분리 후 언리얼 클라이언트에게 소켓 전송
+    #variable.socketio.emit('answer', result)
+
+    # 입력한 텍스트 화면에서 확인할 수 있도록 소켓 전송
     variable.socketio.emit('player_text', {'text': input_word})
 
     return jsonify({'status': 'success'}), 200
