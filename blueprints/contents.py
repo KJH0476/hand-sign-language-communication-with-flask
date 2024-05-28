9076e04b6a2a896a232f804f07b42f9662e0a81f
KJH0476 <148829100+KJH0476@users.noreply.github.com>
2024-05-28 23:51:52 +0900

    feat: 입력한 값 리셋기능 추가

--- blueprints/contents.py
+++ blueprints/contents.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, jsonify
 from service.separate_hangul import separate_text
+from utilities import reconized_hand
+from service import make_hangul
 import variable
 
 contents_bp = Blueprint('contents_bp', __name__)
@@ -21,4 +23,14 @@ def text_show():
     # 입력한 텍스트 화면에서 확인할 수 있도록 소켓 전송
     variable.socketio.emit('player_text', {'text': input_word}, namespace='/web')
 
+    return jsonify({'status': 'success'}), 200
+
+# 입력한 값 리셋
+@contents_bp.route('/reset', methods=['GET'])
+def reset():
+    reconized_hand.store.clear();
+    make_hangul.temp_store.clear();
+    make_hangul.last_chosung_index = None
+    make_hangul.last_jungsung_index = None
+    make_hangul.last_jongsung_index = None
     return jsonify({'status': 'success'}), 200
